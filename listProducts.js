f113b4a5ac2d7e409bbf8cebd651a410df952ceb
George Higbie <gehigbie@gmail.com>
2017-02-10 00:29:56 -0700

    refactored code

--- listProducts.js
+++ listProducts.js
@@ -1,21 +1,13 @@
 var faker = require("faker");
-var fakeProducts = [];
 
 function products(numberOfProducts){
-	var fakeProducts = [];
 	for(var i = 0; i < numberOfProducts; i++){
 		var adj = faker.commerce.productAdjective();
 		var material = faker.commerce.productMaterial();
 		var product = faker.commerce.product();
 		var price = faker.commerce.price();
 		var newFakeProduct = `${adj} ${material} ${product} - $${price}`;
-		fakeProducts.push(newFakeProduct);
-		//return fakeProducts
-	}
-
-	for(var j = 0; j <  fakeProducts.length; j++){
-		console.log(fakeProducts[j]);
-	}
+		console.log(newFakeProduct);
 }
 
 
