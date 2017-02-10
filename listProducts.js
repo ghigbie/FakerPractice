582b2739bec055ff73d413de43520df43214a651
George Higbie <gehigbie@gmail.com>
2017-02-10 00:25:29 -0700

    refactored code

--- listProducts.js
+++ listProducts.js
@@ -4,17 +4,21 @@ var fakeProducts = [];
 function products(numberOfProducts){
 	var fakeProducts = [];
 	for(var i = 0; i < numberOfProducts; i++){
+		var adj = faker.commerce.productAdjective();
+		var material = faker.commerce.productMaterial();
 		var product = faker.commerce.product();
-		fakeProducts.push(product);
-		return fakeProducts
+		var price = faker.commerce.price();
+		var newFakeProduct = `${adj} ${material} ${product} - ${price}`;
+		fakeProducts.push(newFakeProduct;
+		//return fakeProducts
+	}
+
+	for(var j = 0; j <  fakeProducts.length; j++){
+		console.log(fakeProducts[j]);
 	}
 }
 
 
+
 products(10);
-
-
-
-
-console.log(faker());
 
